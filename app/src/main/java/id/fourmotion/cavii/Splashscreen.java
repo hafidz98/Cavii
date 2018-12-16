5ea5fd5dc0fa2552ac11c0bbb1fc06bcbc4e83c2
Hafidz <public.acc78@gmail.com>
2018-12-16 20:28:39 +0700

    commit change Meubah pada tampilan theme app

--- app/src/main/java/id/fourmotion/cavii/Splashscreen.java
+++ app/src/main/java/id/fourmotion/cavii/Splashscreen.java
@@ -1,9 +1,12 @@
 package id.fourmotion.cavii;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -12,8 +15,16 @@ public class Splashscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
+        // Handler delay activity splashscreen
+        byte second = 5;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this, Mainmenu.class));
+                finish();
+            }
+        }, 1000 * second);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
-
-
-
 }
