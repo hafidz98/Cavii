27d6cd9b49dc4d09dfe2d300a61c6d0cff030390
BernardTheFour <bernardctxy1@gmail.com>
2018-12-16 19:18:26 +0700

    Working Search View and Button to call intent

--- app/src/main/java/id/fourmotion/cavii/Mainmenu.java
+++ app/src/main/java/id/fourmotion/cavii/Mainmenu.java
@@ -2,8 +2,13 @@ package id.fourmotion.cavii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class Mainmenu extends AppCompatActivity {
 
@@ -12,10 +17,35 @@ public class Mainmenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
 
-        /*SearchView namaHint = (SearchView) findViewById(R.id.search_engine);
-        namaHint.onActionViewExpanded();
-        namaHint.setIconified(true);*/
+        final String input[] = new String[1];
 
+        //Search View Override
+        SearchView searchEngine = findViewById(R.id.search_engine);
+        searchEngine.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Mengambil data dari Search View
+                input[0] = newText;
+                return false;
+            }
+        });
+
+        //Button Override
+        Button tombolCari = findViewById(R.id.tombolcari);
+        tombolCari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    //Place code here to send intent and data
+                } catch (Exception e) {
+                    Log.i("Error", "Button Error");
+                }
+            }
+        });
     }
 }
