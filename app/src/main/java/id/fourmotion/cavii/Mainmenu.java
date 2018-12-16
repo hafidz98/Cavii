ddf592491c52781c50a085b715c8eb4b20c95b5e
BernardTheFour <bernardctxy1@gmail.com>
2018-12-16 21:05:11 +0700

    Activity menu back-end complete (update ver.2)

--- app/src/main/java/id/fourmotion/cavii/Mainmenu.java
+++ app/src/main/java/id/fourmotion/cavii/Mainmenu.java
@@ -49,17 +49,37 @@ public class Mainmenu extends AppCompatActivity {
         });
 
         // ---------Filter Jenis Override-------------
+        final int selectJenis[] = new int[1];
+        selectJenis[0] = 0;
+
         final Spinner filterJenis = findViewById(R.id.jenis_filter);
 
         String[] jenisItem = new String[]{
-                "Pilih jenis baju",
+                "Pilih jenis baju: ",
                 "Kaos oblong",
                 "Kaos berkerah",
                 "Kemeja lengan pendek"
         };
 
-        final ArrayAdapter<String> jenisArrayAdapter = new ArrayAdapter<>(
-                this, R.layout.support_simple_spinner_dropdown_item, jenisItem);
+        final ArrayAdapter<String> jenisArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, jenisItem) {
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == selectJenis[0]) {
+                    // Set the item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
+                    tv.setTextColor(Color.parseColor("#ffffff"));
+                } else {
+                    // Set the alternate item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
+                    tv.setTextColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
 
         jenisArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         filterJenis.setAdapter(jenisArrayAdapter);
@@ -69,7 +89,7 @@ public class Mainmenu extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //Take data from selected item
                 selectedItemBahan[0] = (String) parent.getItemAtPosition(position);
-
+                selectJenis[0] = parent.getSelectedItemPosition();
             }
 
             @Override
@@ -81,17 +101,35 @@ public class Mainmenu extends AppCompatActivity {
 
         // ---------Filter Jenis Override-------------
         final Spinner filterBahan = findViewById(R.id.bahan_filter);
+        final int selectBahan[] = new int[1];
+        selectBahan[0] = 0;
 
         String[] jenisBahan = new String[]{
-                "Pilih bahan baju",
+                "Pilih bahan baju: ",
                 "Katun",
                 "Sintetis",
                 "Sutra"
         };
 
-        final ArrayAdapter<String> bahanArrayAdapter = new ArrayAdapter<>(
-                this, R.layout.support_simple_spinner_dropdown_item, jenisBahan);
-
+        final ArrayAdapter<String> bahanArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, jenisBahan) {
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == selectBahan[0]) {
+                    // Set the item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
+                    tv.setTextColor(Color.parseColor("#ffffff"));
+                } else {
+                    // Set the alternate item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
+                    tv.setTextColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
         bahanArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         filterBahan.setAdapter(bahanArrayAdapter);
 
@@ -118,7 +156,7 @@ public class Mainmenu extends AppCompatActivity {
                 try {
                     // Place code here to send intent and data
                 } catch (Exception e) {
-                    Log.i("Error", "Button Error");
+                    Log.d("Error", "Sending Error");
                 }
             }
         });
