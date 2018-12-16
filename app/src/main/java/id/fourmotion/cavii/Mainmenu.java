2efba07d98ea8e7f5700b0ecd88771529bf66982
Hafidz <public.acc78@gmail.com>
2018-12-16 15:06:58 +0700

    Initial commit Membuat dan mengatur layout splashscreen dan menu utama

---
+++ app/src/main/java/id/fourmotion/cavii/Mainmenu.java
@@ -0,0 +1,13 @@
+package id.fourmotion.cavii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mainmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+    }
+}
---
+++ app/src/main/java/id/fourmotion/cavii/Splashscreen.java
@@ -0,0 +1,19 @@
+package id.fourmotion.cavii;
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+    }
+
+
+
+}
