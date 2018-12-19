8ab4e6768298e660009a28bfa4d544b5d2c7eb80
Hafidz <public.acc78@gmail.com>
2018-12-19 09:58:44 +0700

    Edit detail content layout name

--- app/src/main/java/id/fourmotion/cavii/DetailContent.java
+++ app/src/main/java/id/fourmotion/cavii/DetailContent.java
@@ -8,6 +8,6 @@ public class DetailContent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_conten);
+        setContentView(R.layout.activity_detail_content);
     }
 }
