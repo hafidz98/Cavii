5b3b02574e2dd829c7e65072683e2e61a291a2e7
BernardTheFour <bernardctxy1@gmail.com>
2018-12-20 14:07:58 +0700

    update ver5.0

--- app/src/main/java/id/fourmotion/cavii/Mainmenu.java
+++ app/src/main/java/id/fourmotion/cavii/Mainmenu.java
@@ -39,6 +39,54 @@ public class Mainmenu extends AppCompatActivity {
         final String selectedItemJenis[] = new String[1];
         final String selectedItemBahan[] = new String[1];
 
+        // ---------Filter Bahan Override-------------
+        bans = db.getBahan();
+        final Spinner filterBahan = findViewById(R.id.bahan_filter);
+        final int selectBahan[] = new int[1];
+        selectBahan[0] = 0;
+
+
+
+        final ArrayAdapter<String> bahanArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, bans) {
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == selectBahan[0]) {
+                    // Set the item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
+                    tv.setTextColor(Color.parseColor("#ffffff"));
+                } else {
+                    // Set the alternate item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
+                    tv.setTextColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
+        bahanArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        filterBahan.setAdapter(bahanArrayAdapter);
+
+        filterBahan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Take data from selected item
+                selectBahan[0] = parent.getSelectedItemPosition();
+                if (selectBahan[0] == 0){
+                    selectedItemBahan[0] = null;
+                } else {
+                    selectedItemBahan[0] = (String) parent.getItemAtPosition(position);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         // ---------Filter Jenis Override-------------
         db = new MyDatabase(this);
         jens = db.getJenis();
@@ -77,8 +125,22 @@ public class Mainmenu extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //Take data from selected item
-                selectedItemJenis[0] = (String) parent.getItemAtPosition(position);
                 selectJenis[0] = parent.getSelectedItemPosition();
+                if (selectJenis[0] == 0){
+                    selectedItemJenis [0] = null;
+
+                    //set false to filter bahan
+                    filterBahan.setSelection(0);
+                    filterBahan.setEnabled(false);
+                    filterBahan.setClickable(false);
+
+                } else {
+                    selectedItemJenis[0] = (String) parent.getItemAtPosition(position);
+
+                    //set true to filter bahan
+                    filterBahan.setEnabled(true);
+                    filterBahan.setClickable(true);
+                }
             }
 
             @Override
@@ -86,59 +148,6 @@ public class Mainmenu extends AppCompatActivity {
 
             }
         });
-
-
-        // ---------Filter Jenis Override-------------
-        bans = db.getBahan();
-        final Spinner filterBahan = findViewById(R.id.bahan_filter);
-        final int selectBahan[] = new int[1];
-        selectBahan[0] = 0;
-
-
-
-        final ArrayAdapter<String> bahanArrayAdapter = new ArrayAdapter<String>(
-                this, R.layout.support_simple_spinner_dropdown_item, bans) {
-            @Override
-            public View getDropDownView(int position, View convertView,
-                                        ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView tv = (TextView) view;
-                if (position == selectBahan[0]) {
-                    // Set the item background color and text
-                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
-                    tv.setTextColor(Color.parseColor("#ffffff"));
-                } else {
-                    // Set the alternate item background color and text
-                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
-                    tv.setTextColor(Color.parseColor("#000000"));
-                }
-                return view;
-            }
-        };
-        bahanArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        filterBahan.setAdapter(bahanArrayAdapter);
-
-        filterJenis.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Take data from selected item
-                selectJenis[0] = parent.getSelectedItemPosition();
-                if (selectJenis[0] == 0){
-                    selectedItemJenis[0] = "tidak_ada";
-                    filterBahan.setSelection(0);
-                    filterBahan.setEnabled(false);
-                    filterBahan.setClickable(false);
-                } else {
-                    selectedItemJenis[0] = (String) parent.getItemAtPosition(position);
-                    filterBahan.setEnabled(true);
-                    filterBahan.setClickable(true);
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-            });
 
         // -----------Search View Override-----------
         final SearchView searchEngine = findViewById(R.id.search_engine);
@@ -148,7 +157,7 @@ public class Mainmenu extends AppCompatActivity {
                 searchEngine.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                     @Override
                     public boolean onQueryTextSubmit(String query) {
-                        input[0] = (String) query;
+                        input[0] = query;
                         return false;
                     }
 
