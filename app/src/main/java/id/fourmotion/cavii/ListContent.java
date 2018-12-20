775c6cd66a12061f7f192b202471412a8c6038bd
BernardTheFour <bernardctxy1@gmail.com>
2018-12-20 17:39:50 +0700

    Terbaru List Content take data and error when data is null

--- app/src/main/java/id/fourmotion/cavii/ListContent.java
+++ app/src/main/java/id/fourmotion/cavii/ListContent.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,29 +16,40 @@ public class ListContent extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private ContentAdapter adapter;
+    private ArrayList<Content> contentArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_content);
 
+
+
+
+        //Take data from extras
+        ArrayList<String> dataEkstra = getIntent().getStringArrayListExtra("dataSearch()");
+
         MyDatabase db = new MyDatabase(this);
 
-
         recyclerView = (RecyclerView) findViewById(R.id.rv_content);
-
-        adapter = new ContentAdapter(db.getSearchKonveksi("Kemeja","Katun"));
-
+        if (dataEkstra.get(0) == null || dataEkstra.get(0).length() <1){
+            try {
+                adapter = new ContentAdapter(db.getSearchKonveksi(dataEkstra.get(1), dataEkstra.get(2)));
+            } catch (Exception e) {
+                //Data tidak ditemukan
+                Toast.makeText(ListContent.this, "Yah konveksi kamu tidak ditemukan", Toast.LENGTH_SHORT).show();
+                contentArrayList = new ArrayList<>();
+                contentArrayList.add(new Content("","","",""));
+                adapter = new ContentAdapter(contentArrayList);
+            }
+        } else {
+            //pake search view
+            //adapter = new ContentAdapter(db.getSearchView(dataEkstra.get(0)));
+        }
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListContent.this);
 
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.setAdapter(adapter);
     }
-    /*
-    void addData(){
-        contentArrayList = new ArrayList<>();
-        contentArrayList.add(new Content("Kahoona Konveksi", "Kemeja","Katun","20000"));
-    }
-    */
 }
