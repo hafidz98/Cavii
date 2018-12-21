7d11b518446ad2282c95a7f9305ac086c99b273e
Hafidz <public.acc78@gmail.com>
2018-12-21 09:46:00 +0700

    Added custom logo and icon for app

--- app/src/main/java/id/fourmotion/cavii/Splashscreen.java
+++ app/src/main/java/id/fourmotion/cavii/Splashscreen.java
@@ -8,12 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.squareup.picasso.Picasso;
+
 public class Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        //Picasso.with(image.getContext()).load(storagePath).fit().centerCrop().into(image);
+
+
 
         // Handler delay activity splashscreen
         byte second = 3;
