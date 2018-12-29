c433913e114556eb1a3bcbe3bd57ab879b20df30
BernardTheFour <bernardctxy1@gmail.com>
2018-12-30 00:50:08 +0700

    Big Update

--- app/src/main/java/id/fourmotion/cavii/ListContent.java
+++ app/src/main/java/id/fourmotion/cavii/ListContent.java
@@ -1,26 +1,20 @@
 package id.fourmotion.cavii;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.support.annotation.NonNull;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 import id.fourmotion.cavii.Adapter.ContentAdapter;
@@ -32,50 +26,152 @@ public class ListContent extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ContentAdapter adapter;
     private ArrayList<Content> contentArrayList;
-    private ArrayList<String> dataEkstra;
     private MyDatabase db;
-    String id;
+    private String inputSearch, selectedItemJenis, selectedItemBahan, id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_content);
 
-        //Take data from extras
-        dataEkstra = getIntent().getStringArrayListExtra("dataSearch()");
+        // -----------Final data to send via intent-------
+        ArrayList<String> jens, bans;
 
-        //Put textview categories
-        TextView filterJenis = findViewById(R.id.jenis_filter);
-        TextView filterBahan = findViewById(R.id.bahan_filter);
-
-        String forJenis, forBahan;
-
-
-        if (dataEkstra.get(1) == null) {
-            forJenis = "Jenis Baju: Semua Jenis";
-        } else {
-            forJenis = "Jenis Baju: " + dataEkstra.get(1);
-        }
-
-        if (dataEkstra.get(2) == null) {
-            forBahan = "Bahan Baju: Semua Bahan";
-        } else {
-            forBahan = "Bahan Baju: " + dataEkstra.get(2);
-        }
-
-        filterJenis.setText(forJenis);
-        filterBahan.setText(forBahan);
-
+        // ---------Filter Bahan Override-------------
         db = new MyDatabase(this);
+        bans = db.getBahan();
+        final Spinner filterBahan = findViewById(R.id.bahan_filter);
+        final int selectBahan[] = new int[1];
+        selectBahan[0] = 0;
 
+        final ArrayAdapter<String> bahanArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, bans) {
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == selectBahan[0]) {
+                    // Set the item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
+                    tv.setTextColor(Color.parseColor("#ffffff"));
+                } else {
+                    // Set the alternate item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
+                    tv.setTextColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
+        bahanArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        filterBahan.setAdapter(bahanArrayAdapter);
+
+        filterBahan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Take data from selected item
+                selectBahan[0] = parent.getSelectedItemPosition();
+                if (selectBahan[0] == 0) {
+                    selectedItemBahan = null;
+                } else {
+                    selectedItemBahan = (String) parent.getItemAtPosition(position);
+                    tampilData();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        // ---------Filter Jenis Override-------------
+        jens = db.getJenis();
+        final int selectJenis[] = new int[1];
+        selectJenis[0] = 0;
+        final Spinner filterJenis = findViewById(R.id.jenis_filter);
+
+        final ArrayAdapter<String> jenisArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, jens) {
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == selectJenis[0]) {
+                    // Set the item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#00A2E9"));
+                    tv.setTextColor(Color.parseColor("#ffffff"));
+                } else {
+                    // Set the alternate item background color and text
+                    tv.setBackgroundColor(Color.parseColor("#ffffff"));
+                    tv.setTextColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
+
+
+        jenisArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        filterJenis.setAdapter(jenisArrayAdapter);
+
+        filterJenis.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Take data from selected item
+                selectJenis[0] = parent.getSelectedItemPosition();
+                if (selectJenis[0] == 0) {
+                    selectedItemJenis = null;
+
+                    //set false to filter bahan
+                    filterBahan.setSelection(0);
+                    selectBahan[0] = 0;
+                    filterBahan.setEnabled(false);
+                    filterBahan.setClickable(false);
+
+                } else {
+                    selectedItemJenis = (String) parent.getItemAtPosition(position);
+
+                    //set true to filter bahan
+                    filterBahan.setEnabled(true);
+                    filterBahan.setClickable(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // -----------Search View Override-----------
+        final SearchView searchEngine = findViewById(R.id.search_engine);
+        searchEngine.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                tampilData();
+                inputSearch = s;
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                return false;
+            }
+        });
+    }
+
+    void tampilData(){
         recyclerView = findViewById(R.id.rv_content);
 
 
-        if (dataEkstra.get(0) == null || dataEkstra.get(0).length() < 1) {
+        if (inputSearch == null || inputSearch.length() < 1) {
             try {
                 defaultData();
                 //adapter.setContext(ListContent.this);
-                adapter = new ContentAdapter(db.getSearchKonveksi(dataEkstra.get(1), dataEkstra.get(2)));
+                adapter = new ContentAdapter(db.getSearchKonveksi(selectedItemJenis, selectedItemBahan));
             } catch (Exception e) {
                 //Data tidak ditemukan
                 Toast.makeText(ListContent.this, "Yah konveksi kamu tidak ditemukan", Toast.LENGTH_SHORT).show();
@@ -86,7 +182,6 @@ public class ListContent extends AppCompatActivity {
             //adapter = new ContentAdapter(db.getSearchView(dataEkstra.get(0)));
         }
 
-
         //show data from database
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListContent.this);
         recyclerView.setLayoutManager(layoutManager);
@@ -96,11 +191,6 @@ public class ListContent extends AppCompatActivity {
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this, recyclerView, new ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                //WARNING!!! Ganti ke ID from content
-                //String sendData = db.getSearchKonveksi(dataEkstra.get(1), dataEkstra.get(2)).get(position).get_id(); //
-
-                //Toast.makeText(ListContent.this, db.getId(position), Toast.LENGTH_SHORT).show();
-
                 //Send Intent
                 Intent toDetailContent = new Intent(ListContent.this, DetailContent.class);
                 toDetailContent.putExtra("trans_ID()", db.getId(position));
