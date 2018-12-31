830bc7ad8a2b6952fdf9e86f7d303439334244d3
BernardTheFour <bernardctxy1@gmail.com>
2018-12-31 12:27:13 +0700

    comnit2

---
+++ app/src/main/java/id/fourmotion/cavii/MenuFav.java
@@ -0,0 +1,13 @@
+package id.fourmotion.cavii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuFav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menufav);
+    }
+}
