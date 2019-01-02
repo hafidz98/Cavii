b0acb0fec4ac82a5f3d367843be66f36b95b4416
Hafidz <public.acc78@gmail.com>
2019-01-02 20:39:48 +0700

    Ubah dan tambah listing kode untuk menampilkan data favorit

--- app/src/main/java/id/fourmotion/cavii/MenuFav.java
+++ app/src/main/java/id/fourmotion/cavii/MenuFav.java
@@ -3,21 +3,61 @@ package id.fourmotion.cavii;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import id.fourmotion.cavii.Adapter.ContentAdapter;
+import id.fourmotion.cavii.Helper.MyDatabase;
+import id.fourmotion.cavii.Model.Content;
 
 public class MenuFav extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MyDatabase db;
+    private ArrayList<Content> contentArrayList;
+    private ContentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menufav);
 
+        getSupportActionBar().setTitle("Favorite");
+
         //Button//
         ImageButton favButton = findViewById(R.id.menu_fav);
         favButton.setSelected(true);
 
+        tampilData();
+
+    }
+
+    void tampilData(){
+        recyclerView = findViewById(R.id.rv_content_fav);
+        try{
+            db = new MyDatabase(MenuFav.this);
+            adapter = new ContentAdapter(db.getSelectFavorite());
+        }catch (Exception e){
+            Toast.makeText(this, "Yah konveksi kamu tidak ditemukan", Toast.LENGTH_SHORT).show();
+            defaultData();
+            db.refreshId();
+        }
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void defaultData() {
+        contentArrayList = new ArrayList<>();
+        contentArrayList.add(new Content(null, "", "", "", "", "", "", "", "", "", ""));
+        adapter = new ContentAdapter(contentArrayList);
     }
 
     @Override
